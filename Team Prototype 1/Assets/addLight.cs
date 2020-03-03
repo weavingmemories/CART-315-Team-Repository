d219d9d6ab1189ffd76b0082996d9c1453c22b5c
Eugene N. Fournier <eugene.n.fournier@gmail.com>
2020-03-03 16:16:53 -0500

    Added light prefabs

---
+++ Team Prototype 1/Assets/addLight.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class addLight : MonoBehaviour
+{
+    public GameObject thecamera;
+    public GameObject create;
+    static public Queue<GameObject> lightCollection = new Queue<GameObject>(); 
+    static public int lightCount = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+  
+        if (Input.GetMouseButtonDown(1))
+        {
+            RaycastHit hit;
+            Ray ray = thecamera.GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {
+                if(lightCount >= 3)
+                {
+                    Destroy(lightCollection.Dequeue());
+                    lightCount -= 1;
+                    if (addLight.lightCount < 0)
+                    {
+                        addLight.lightCount = 0;
+                    }
+                }
+                lightCollection.Enqueue(GameObject.Instantiate(create, hit.point, Quaternion.identity));
+                lightCount += 1;
+            }
+        }
+
+    }
+}
