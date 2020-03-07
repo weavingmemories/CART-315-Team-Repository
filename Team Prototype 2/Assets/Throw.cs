5b863ebbb40b170723b95079894be41b0edc24ca
Eugene N. Fournier <eugene.n.fournier@gmail.com>
2020-03-06 23:27:33 -0500

    Added standard assets

---
+++ Team Prototype 2/Assets/Throw.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Throw : MonoBehaviour
+{
+	public GameObject platform;
+	public GameObject camera;
+	public ModeSwitch modeSwitch;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetMouseButtonDown(0) && modeSwitch.nightmode == false)
+		{
+			GameObject clone = GameObject.Instantiate(platform, transform.position, platform.transform.rotation);
+
+			clone.GetComponent<Rigidbody>().AddForce(camera.transform.forward*50,ForceMode.Impulse);
+		}
+
+	}
+}
---
+++ Team Prototype 2/Assets/materialize.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class materialize : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    public Transform platform;
+    public int collisionCount = 0;
+    public bool deactivate = false;
+    public Material deadMaterial;
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+        collisionCount++;
+        if (collisionCount >= 4)
+        {
+            deactivate = true;
+            gameObject.GetComponent<MeshRenderer>().material = deadMaterial;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(deactivate == false)
+        {
+            if (Input.GetMouseButtonDown(1))
+            {
+                if (collisionCount > 1 && collisionCount < 4)
+                {
+                    Quaternion rotation = gameObject.transform.rotation;
+                    Vector3 position = gameObject.transform.position;
+                    Instantiate(platform, position, rotation);
+                    Destroy(gameObject);
+                }
+                
+            }
+        }
+       
+    }
+}
