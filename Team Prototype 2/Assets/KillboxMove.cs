30bac89f93c9c9810023b61dd0e608147dc768a6
Macey McCormick <red-rainbows@hotmail.com>
2020-03-07 21:46:20 -0500

    Added base "level" as well as rising killbox

---
+++ Team Prototype 2/Assets/KillboxMove.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillboxMove : MonoBehaviour
+{
+    private float speed;
+    bool mode1Speed;
+    bool mode2Speed;
+
+    private void Start()
+    {
+        mode1Speed = true;
+        mode2Speed = false;
+    }
+
+    void FixedUpdate()
+    {
+        if (mode1Speed == true && mode2Speed == false)
+        {
+            transform.Translate(Vector2.up * 0.2f * Time.deltaTime);
+        }
+
+        if (mode2Speed == true && mode1Speed == false)
+        {
+            transform.Translate(Vector2.up * 5f * Time.deltaTime);
+        }
+        
+    }
+
+     void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Mode1"))
+        {
+            mode1Speed = true;
+            mode2Speed = false;
+        }
+        else if (other.CompareTag("Mode2"))
+        {
+            mode1Speed = false;
+            mode2Speed = true;
+        }
+    }
+}
---
+++ Team Prototype 2/Assets/RestartLevel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartLevel : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Scene scene;
+            scene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(scene.name);
+         
+        }
+    }
+}
