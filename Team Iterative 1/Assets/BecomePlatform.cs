08c20d1ae5aa7509eb0060cf02d417047c153f77
Eugene N. Fournier <42948298+Cadere@users.noreply.github.com>
2020-04-07 21:59:24 -0400

    Added throwing platforms

---
+++ Team Iterative 1/Assets/BecomePlatform.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BecomePlatform : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+
+    public Transform platform;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(1))
+        {
+                
+            Quaternion rotation = gameObject.transform.rotation;
+            Vector3 position = gameObject.transform.position;
+            Instantiate(platform, position, rotation);
+            Destroy(gameObject);
+
+        }
+
+    }
+}
