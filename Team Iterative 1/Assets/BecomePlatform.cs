f2cf5a7ac486635af66a653cf8114da8fba09e20
Eugene N. Fournier <42948298+Cadere@users.noreply.github.com>
2020-04-07 23:31:06 -0400

    switches now change colors when hit

--- Team Iterative 1/Assets/BecomePlatform.cs
+++ Team Iterative 1/Assets/BecomePlatform.cs
@@ -13,7 +13,6 @@ public class BecomePlatform : MonoBehaviour
 
     public Transform platform;
 
-
     // Update is called once per frame
     void Update()
     {
---
+++ Team Iterative 1/Assets/lightChange.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lightChange : MonoBehaviour
+{
+    public GameObject thisLock;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (thisLock.tag == "unlocked")
+        {
+            this.gameObject.GetComponent<Light>().color = Color.green;
+        }
+
+    }
+}
