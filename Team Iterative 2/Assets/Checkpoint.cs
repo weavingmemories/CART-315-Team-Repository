0549cae4d2379a6896d164bf1a15ef2ed566efac
Macey McCormick <red-rainbows@hotmail.com>
2020-04-10 14:36:48 -0400

    trying checkpoints

---
+++ Team Iterative 2/Assets/Checkpoint.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    [SerializeField] public Transform respawnPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        
+        if (other.CompareTag("Player"))
+        {
+            respawnPoint = this.gameObject.transform;
+        }
+    }
+}
--- Team Iterative 2/Assets/RestartLevel.cs
+++ Team Iterative 2/Assets/RestartLevel.cs
@@ -1,17 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+// If we end the game and want the player to restart, restarting the whole game will be key.
+// using UnityEngine.SceneManagement;
 
 public class RestartLevel : MonoBehaviour
 {
+    public GameObject checkpoint;
+    [SerializeField] private Transform player;
+    public Checkpoint checkpointScript;
+
+    private void Start()
+    {
+        checkpointScript = checkpoint.GetComponent<Checkpoint>();
+    }
+
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            Scene scene;
-            scene = SceneManager.GetActiveScene();
-            SceneManager.LoadScene(scene.name);
+            //   Scene scene;
+            //   scene = SceneManager.GetActiveScene();
+            //   SceneManager.LoadScene(scene.name);
+            
+            player.transform.position = checkpointScript.respawnPoint.transform.position;
+            Physics.SyncTransforms();
 
         }
     }
